6a7a9761b4186dba931c1eba8924770e289f4426
sneha-siju <offcsnehasiju@gmail.com>
2025-08-22 02:54:07 +0530

    Add files via upload

---
+++ inputname.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  std::string name;
+  std::cout<<"Enter name:";
+  std::cin>>name;
+  cout<<"Namah shivayah "<<name<<"!";
+  return 0;
+  }
