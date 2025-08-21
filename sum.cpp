d7cd82789a8179a21f55b262434ddcc22a5f9f2d
sneha-siju <offcsnehasiju@gmail.com>
2025-08-22 02:47:11 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y,sum;
+  std:: cout<<"Enter two numbers:";  
+  std::cin>>x>>y;                        //inputting both numbers
+  sum=x+y;                              //declares sum
+  std:: cout<<"The sum is:"<<sum;
+  return 0;
+  }
+  
