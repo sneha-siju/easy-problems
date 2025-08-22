1653e7b0c28ecb71edc4eb1f2dff46df7b135903
sneha-siju <offcsnehasiju@gmail.com>
2025-08-23 00:46:48 +0530

    Add files via upload

---
+++ areaofrectangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int len,wid,area;                                           //introducing length, width and area
+  std::cout<<"Enter length and width:";
+  std::cin>>len>>wid;                                   
+  area= len*wid;                                        // defining area
+ std:: cout<<"area of rectangle is :"<<area;
+  return 0;
+  }
