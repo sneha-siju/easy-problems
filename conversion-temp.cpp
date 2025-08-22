38c0e735ffb724a69ed8efee9843074867472ace
sneha-siju <offcsnehasiju@gmail.com>
2025-08-23 00:56:06 +0530

    Add files via upload

---
+++ conversion-temp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int c , f;
+  std::cout<<"Enter number in Celsius form:";
+  std::cin>>c;
+  f=((9/5)*c)+32;                                  //equation to convert Celsius to Fahrenheit
+  std::cout<<"Celsius converted to Fahrenheit :"<<f;
+  return 0;
+  }
+  
+  
