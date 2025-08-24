e1505578025d56de18f2263f56e4cef9ad860747
sneha-siju <offcsnehasiju@gmail.com>
2025-08-24 22:14:38 +0530

    Add files via upload

---
+++ avg-3numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y,z,avg;
+  cout<<"Enter three numbers: ";
+  cin>>x>>y>>z;
+  avg=(x+y+z)/3;
+  cout<<"The average of given three numbers is: "<<avg;
+  return 0;
+  }
+  
