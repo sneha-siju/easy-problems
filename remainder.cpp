9b7a365ccd4fbcee501819e03e7eab8811bed953
sneha-siju <offcsnehasiju@gmail.com>
2025-08-24 21:37:56 +0530

    Add files via upload

---
+++ remainder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y,z;
+  cout<<"Enter two numbers:";
+  cin>>x>>y;
+  z=x%y;
+  cout<<"The remainder when "<<x<<" and "<<y<<" are divided is"<<z;
+  return 0;
+  }
