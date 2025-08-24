ae0476540d6e6ecde0adbfca90a333a4eb0168fa
sneha-siju <offcsnehasiju@gmail.com>
2025-08-24 22:10:22 +0530

    Add files via upload

---
+++ even-odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x;
+  cout<<"Enter a number: ";
+  cin>>x;
+  if(x%2==0)
+  cout<<x<<" is an even number.";
+  if(x%2!=0)
+  cout<<x<<" is an odd number.";
+  return 0;
+  }
