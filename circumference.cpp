326d97d37475013035b852ed58842a12b7febd73
sneha-siju <offcsnehasiju@gmail.com>
2025-08-26 22:26:48 +0530

    Add files via upload

---
+++ circumference.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+  int r;
+  float cir;
+  cout<<"Enter radius";
+  cin>>r;
+  cir=2*(3.14)*r;
+  cout<<"Circumference of circle with radius "<<r<<" is "<<cir;
+  return 0;
+  }
