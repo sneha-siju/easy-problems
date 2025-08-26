3b24de080996bfe48dba21415a96b0984adb2e7c
sneha-siju <offcsnehasiju@gmail.com>
2025-08-26 21:42:54 +0530

    Add files via upload

---
+++ number-squared.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,sqr;
+  cout<<"Enter a number:";
+  cin>>x;
+  sqr=x*x;
+  cout<<"The square of "<<x<<" is "<<sqr;
+  return 0;
+  }
+  
+	
