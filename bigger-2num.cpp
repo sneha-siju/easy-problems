d4a18d2d83b8b3e2150078831d90ae86901faa57
sneha-siju <offcsnehasiju@gmail.com>
2025-08-26 20:55:28 +0530

    Add files via upload

---
+++ bigger-2num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y;
+  cout<<"Enter two numbers:";
+  cin>>x>>y;
+  if(x>y)
+  cout<<"The bigger number is "<<x;
+  else
+  cout<<y<<"is the bigger element";
+  return 0;
+  }
+  
