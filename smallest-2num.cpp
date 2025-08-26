01845c5ca45a164c3cb5a5cbfd9b73c9de48c430
sneha-siju <offcsnehasiju@gmail.com>
2025-08-26 21:37:56 +0530

    Add files via upload

---
+++ smallest-2num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y;
+  cout<<"Enter two numbers:";
+  cin>>x>>y;
+  if(x<y)
+  cout<<x<<" is the smallest number";
+  else
+  cout<<y<<" is the smallest number";
+  return 0;
+  }
