d25b1a31ccac2ca779d1bd6cc17be60f165e84fc
sneha-siju <offcsnehasiju@gmail.com>
2025-08-28 00:17:46 +0530

    Add files via upload

---
+++ ascii-char.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char c;
+  cout<<"Enter a character: ";
+  cin>>c;
+  int value= (int) c;
+  cout<<"ASCII value of "<<c<<" is "<<value;
+  return 0;
+  }
