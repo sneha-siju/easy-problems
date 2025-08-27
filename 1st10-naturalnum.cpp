320d6eb3ce2252dceb1d2e53edbfb22fa6d762a1
sneha-siju <offcsnehasiju@gmail.com>
2025-08-28 00:27:56 +0530

    Add files via upload

---
+++ 1st10-naturalnum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i;
+  cout<<"Press i:";
+  cin>>i;
+   cout<<"The first 10 natural numbers: ";
+  for ( i=1;i<11;i++)
+  cout<< i <<"\n";
+  return 0;
+  }
