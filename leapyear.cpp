1591c4c40cd125dbe98d5c415f7f1d36da6e6959
sneha-siju <offcsnehasiju@gmail.com>
2025-08-28 18:19:09 +0530

    Add files via upload

---
+++ leapyear.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int y;
+  cout<<"Enter a year: ";
+  cin>>y;
+  if(y%4==0){
+    if(y%100!=0){
+  cout<<y<<" is a leap year.";}
+  }
+  else
+  cout<<y<<" is not a leap year.";
+  return 0;
+  }
